5ae414cf6c2403d59c6688ca64519b2fbe3357ef
Amanda Simão <amanda.simao@aluno.ifce.edu.br>
2025-03-21 14:32:25 -0300

    Atualizar o chave.sh

--- chave.sh
+++ chave.sh
@@ -1,8 +1,8 @@
 git config --global user.name "Amanda da Silva Simão"
 git config --global user.email "amanda.simao@aluno.ifce.edu.br"
 
-cp ~/.ssh/20231321000039 ~/ssh/id_ed25519
-cp ~/.ssh/20231321000039.pub ~pub/.ssh/id_ed25519.pub
+cp ~/.ssh/20231321000039 ~/.ssh/id_ed25519
+cp ~/.ssh/20231321000039.pub ~/.ssh/id_ed25519.pub
 
 chmod 600 ~/.ssh/id_ed25519
 chmod 644 ~/.ssh/id_ed25519.pub
