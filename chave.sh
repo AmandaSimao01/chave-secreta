169bc1f4d4a2caf67f474e64530f5fb269beea01
Amanda Simão <amanda.simao@aluno.ifce.edu.br>
2025-03-21 14:11:56 -0300

    Add files via upload

---
+++ chave.sh
@@ -0,0 +1,10 @@
+git config --global user.name "Amanda da Silva Simão"
+git congig --global user.email "amanda.simao@aluno.ifce.edu.br"
+
+cp ~/.ssh/20231321000039 ~/ssh/id_ed25519
+cp ~/.ssh/20231321000039.pub ~pub/.ssh/id_ed25519.pub
+
+chmod 600 ~/.ssh/id_ed25519
+chmod 644 ~/.ssh/id_ed25519.pub
+
+echo "Configuração Concluída!"
